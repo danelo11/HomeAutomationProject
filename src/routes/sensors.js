a1a7bc17817ea458c5b0f2d10a6c5fc3ce281959
Danel <danelo@users.noreply.github.com>
2020-06-13 13:29:23 +0200

    Rutas

--- src/routes/sensors.js
+++ src/routes/sensors.js
@@ -1,15 +1,24 @@
 const express = require('express');
 const router = express.Router();
-const {renderMainPage, renderReal, renderConfig, renderMoni, renderSensores} = require('../controllers/controlador');
+const {renderMainPage, 
+    renderReal,
+    renderConfig, 
+    renderMoni, 
+    renderSensores, 
+    showMeasurements,
+} = require('../controllers/controlador');
+const {isAuthenticated} = require('../auxiliary/authenticated');
 
 //Routes
-router.get('/index',renderMainPage);
+router.get('/index', renderMainPage);
 
 router.get('/sensores', renderSensores);
 
 router.get('/tiemporeal', renderReal);
 
 router.get('/moni', renderMoni);
+
+router.get('/moni/filters', showMeasurements);
 
 router.get('/config', renderConfig);
 
--- src/routes/users.js
+++ src/routes/users.js
@@ -20,7 +20,17 @@ router.post('/users/signin', passport.authenticate('local', {
 router.post('/users/signup', async function(request, response, next){
     const { name, email, password, confpassword} = request.body;
     if (name.length < 1 || email.length <1 || password.length <1 || confpassword.length <1 || password != confpassword || password.lenght < 6){
-        response.render('users/signup', {layout: false});
+        const errors = [];
+        if (name.length < 1 || email.length <1 || password.length <1 || confpassword.length <1){
+            errors.push({text: "Asegurese de rellenar todos los campos"})
+        }
+        if(password != confpassword){
+            errors.push({text: "Las contraseñas no coinciden"});
+        }
+        if(password.lenght < 6){
+            errors.push({text: "La contraseña debe contener al menos 6"});
+        }
+        response.render('users/signup', errors, {layout: false});
     }else{
         const coincidencia = await usuario.findOne({email: email});
         if(coincidencia){
